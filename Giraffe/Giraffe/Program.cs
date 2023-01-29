78e86032d72b6c9b87ff801886406311136b1b03
KatSchultz <ks.katieschultz@gmail.com>
2023-01-28 20:37:32 -0500

    initial commit

---
+++ Giraffe/Giraffe/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string words = "Hello Mr. Bond.";
+        string flipped = "";
+
+        char[] splitWords = words.ToCharArray();
+        
+        for (int i = 0; i < splitWords.Length; i++)
+        {
+            string temp = splitWords[i].ToString();
+            string inverse = "";
+            if (temp == temp.ToLower())
+               
+                inverse = temp.ToUpper();
+            if (temp == temp.ToUpper())
+                inverse = temp.ToLower();
+
+            flipped += inverse;
+        }
+        Console.WriteLine(flipped);
+    }
+}
